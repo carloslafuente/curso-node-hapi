308d5de305e27064584d04b102764eef2d78053a
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-18 11:33:01 -0400

    Refactorizando el index

--- index.js
+++ index.js
@@ -17,44 +17,53 @@ const server = hapi.server({
   },
 });
 
+const declarePlugins = async () => {
+  await server.register(inert);
+  await server.register(vision);
+};
+
+const initRoutes = async () => {
+  server.route({
+    method: 'GET',
+    path: '/home',
+    handler: (req, h) => {
+      return h.view('index', {
+        title: 'Home',
+      });
+    },
+  });
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        index: ['index.html'],
+      },
+    },
+  });
+};
+
+const initViews = async () => {
+  server.views({
+    engines: {
+      hbs: handlebars,
+    },
+    relativeTo: __dirname,
+    path: 'views',
+    layout: true,
+    layoutPath: 'views',
+  });
+};
+
 const init = async () => {
   try {
-    // Registrar los plugins que HAPI usara,
-    // en este caso inert para subir archivos estaticos
-    await server.register(inert);
-    await server.register(vision);
-
-    server.views({
-      engines: {
-        hbs: handlebars,
-      },
-      relativeTo: __dirname,
-      path: 'views',
-      layout: true,
-      layoutPath: 'views',
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/home',
-      handler: (req, h) => {
-        // Gracias a inert accedemos al metodo file
-        return h.view('index', {
-          title: 'Home',
-        });
-      },
-    });
-    // Ruta para servir los archivos estaticos
-    server.route({
-      method: 'GET',
-      path: '/{param*}',
-      handler: {
-        directory: {
-          path: '.',
-          index: ['index.html'],
-        },
-      },
-    });
+    // Registrar los plugins que HAPI usara
+    await declarePlugins();
+    // Iniciamos las rutas
+    await initRoutes();
+    // Iniciamos las vistas
+    await initViews();
     // Iniciamos el servidor
     await server.start();
   } catch (error) {
@@ -66,15 +75,3 @@ const init = async () => {
 };
 
 init();
-
-// Servidor: Unidad basica y principal del framework, Creamos el servidor
-// Ruta: Define puntos de interaccion para la aplicacion
-// Definimos el method: Es el metodo HTTP por el cual un Browser o un cliente
-// debe acceder a la ruta
-// Definimos el path: Define parte de la url que sera accedida por el cliente
-// Definimos el handler: Funcion que se ejecuta cuando se accede a la ruta
-// (QUE ES LO QUE SE HARA CUANDO SE ACCEDA A LA RUTA)
-// h es una coleccion de utilidades y propiedades relativas a enviar
-// informacion de respuesta
-// h.response: Crea un objeto de respuesta
-// h.redirect: Redirecciona una peticion
